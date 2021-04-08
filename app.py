16e8e39cded36d10dec2ad7f76bacf695406f2b5
mbrandal <brandalmartin@gmail.com>
2021-04-08 14:42:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- app.py
+++ app.py
@@ -7,39 +7,43 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  #/tmp// i en tmp folder evt
 db = SQLAlchemy(app)
 
-class BotModel(db.Model):
-    bot_id = db.Column(db.Integer, primary_key=True)
+class UserModel(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    rooms = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f"Bot(name = {name}, rooms = {rooms})"
+        return f"User(name = {name})"
     
+class RoomModel(db.Model):
+    room_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f"Room(name = {name})"
 db.drop_all()
 db.create_all()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', values=BotModel.query.all())
+    return render_template('index.html', values=UserModel.query.all())
 
 @app.route("/api/users", methods=['GET', 'POST'])
 def test():
     if request.method == 'POST':
         name=request.form['name']
-        rooms=request.form['rooms']
-        bot_id=BotModel(name=name, rooms=rooms)
-        bot = BotModel(bot_id=bot_id.bot_id, name=name, rooms=rooms)
-        db.session.add(bot)
+        user_id=UserModel(name=name)
+        user = UserModel(user_id=user_id.user_id, name=name)
+        db.session.add(user)
         db.session.commit()
-        return render_template('index.html', values=BotModel.query.all())
-    else: return render_template('index.html', values=BotModel.query.all())
+        return render_template('index.html', values=UserModel.query.all())
+    else: return render_template('index.html', values=UserModel.query.all())
 
 @app.route("/api/user/<int:user_id>")
 def delete(user_id):
     try:
-        bot = BotModel.query.filter_by(bot_id=user_id).delete()
+        user = UserModel.query.filter_by(user_id=user_id).delete()
         db.session.commit()
-        return render_template('index.html', values=BotModel.query.all())
+        return render_template('index.html', values=UserModel.query.all())
     except: abort(404, message="Bot ID is not valid")
 
 if __name__ == "__main__":
