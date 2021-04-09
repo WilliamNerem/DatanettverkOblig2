4587511ad1ed49240c93eb1db900d91e451519d9
WilliamNerem <william-kn@hotmail.com>
2021-04-09 14:38:55 +0200

    s

--- client.py
+++ client.py
@@ -8,4 +8,8 @@ BASE = "http://127.0.0.1:5000/"
 
 urllib.request.urlopen(BASE + "api/users/" + name)
 urllib.request.urlopen(BASE + "api/rooms/" + roomname)
-urllib.request.urlopen(BASE + "api/room/1/users")
+for i in range(100):
+    try:
+        urllib.request.urlopen(BASE + "api/room/" + str(i) + "/users")
+    except:
+        pass
