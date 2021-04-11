84949c22322b2c727f01c52e1cbdf08faecffaa7
mbrandal <brandalmartin@gmail.com>
2021-04-11 21:04:09 +0200

    Laget enkelt botprogram og koblet det opp mot app.py
    Fjernet 2 parametere på client og la bare til botnavn istedet

---
+++ bot.py
@@ -0,0 +1,14 @@
+import random
+
+def alice():
+    return "I%20think%20botting%20sounds%20great!"
+
+def bob():
+    return "I%20would%20like%20to%20bob!"
+    
+    
+def dora(): 
+    return "YES!%20I%20own!"
+
+def chuck():
+    return "I%20dont%20care!"
--- client.py
+++ client.py
@@ -1,19 +1,24 @@
 import urllib.request
 import urllib.error
 import sys
+import bot
 
 #Når user er i et chat room og en "bot" kjøres, fucker det seg opp. I tillegg vil man få error om man prøver å sende en ny mld
 
-name = sys.argv[1]
-roomname = sys.argv[2]
+
+bots = (bot.alice, bot.bob, bot.dora, bot.chuck)
+curbot = None
+for b in bots:
+    if sys.argv[1] == b.__name__:
+        curbot = b
 
 BASE = "http://127.0.0.1:5000/"
 
-urllib.request.urlopen(BASE + "api/users/" + name)
-urllib.request.urlopen(BASE + "api/rooms/" + roomname)
+urllib.request.urlopen(BASE + "api/users/" + curbot.__name__)
+urllib.request.urlopen(BASE + "api/rooms/" + curbot.__name__ +"s%20room")
 for i in range(10):
     try:
         urllib.request.urlopen(BASE + "api/room/" + str(i) + "/users")
-        urllib.request.urlopen(BASE + "/api/room/messages/" + "Halla%20jeg%20vil%20ha%20deg%20Martin")
+        urllib.request.urlopen(BASE + "/api/room/messages/" + curbot())
     except:
         pass
